03a6f8fab3e57d00bc43dc1ed588d8c9277f7ac5
Anders <1086088@ucn.dk>
2021-11-05 02:38:52 +0100

    work on template

--- src/views/ProgramTemplate.vue
+++ src/views/ProgramTemplate.vue
@@ -1,12 +1,43 @@
 <template>
     <main>
         <section v-if="foundData">
+            <img :src="posterImg" alt="">
             <h1>
-                Template
+                {{ idData.title }}
             </h1>
-            {{ idData.title }}
+            <h2>
+                {{ genreArray }}
+            </h2>
+            <h2>
+                {{ actorArray }}
+            </h2>
+            <h2>
+                {{ directorArray }}
+            </h2>
+            <p>
+                {{ idData.description }}
+            </p>
+            <ul>
+                <li>
+                    {{ idData.plprogram$year }}
+                </li>
+                <li>
+                    <!-- Cover -->
+                    
+                </li>
+                <li>
+                    <!-- Backdrop -->
+                   
+                </li>
+                <li>
+                    {{ idData.plprogram$thumbnails }}
+                </li>
+            </ul>
+            <div v-for="(image, index) in idData.plprogram$thumbnails" :key="index">
+                <img :src="image.plprogram$url" :alt="index">
+            </div>
 
-            <router-link to="/program/229503528355">Link</router-link>
+            
         </section>
 
         <!-- V-if is used insted of v-else, because it should be loaded before the GET-request is done and that there is an error -->
@@ -32,7 +63,50 @@ export default {
         updateError: function () {
             // Just a tiny thing to change in the error message, should the user not know what is meant with an object.
             return this.idData.description.replace("object", "a program")
-        }
+        },
+        genreArray: function () {
+            // These [3]Array's are for sorting through the content and pick out what should be displayed.
+            let newArray = [];
+            this.idData.plprogram$tags.forEach(genre => {
+                newArray.push(genre.plprogram$title)
+            });
+            return newArray;
+        },
+        directorArray: function () {
+            let newArray = [];
+            this.idData.plprogram$credits.forEach(director => {
+                if (director.plprogram$creditType == "director") {
+                    newArray.push(director.plprogram$personName)
+                }
+            });
+            return newArray;
+        },
+        actorArray: function () {
+            let newArray = [];
+            this.idData.plprogram$credits.forEach(actor => {
+                if (actor.plprogram$creditType == "actor") {
+                    newArray.push(actor.plprogram$personName)
+                }
+            });
+            return newArray;
+        },
+        posterImg: function () {
+             let poster = null;
+
+            // This worked to find the poster img, but it seems a lot of the URL's are either missing or not working, so I choose a image-size all programs should have.
+            // for(const key in this.idData.plprogram$thumbnails) {
+            //     if(this.idData.plprogram$thumbnails[key].plprogram$assetTypes[0] == "Poster") {
+            //         poster = this.idData.plprogram$thumbnails[key].plprogram$url;
+            //     }
+            // }
+            // Loops through all the keys in the object looking for a match, and then pick the URL from the correct key.
+            for(const key in this.idData.plprogram$thumbnails) {
+                if(key == "orig-594x408") {
+                    poster = this.idData.plprogram$thumbnails[key].plprogram$url;
+                }
+            }
+            return poster;
+        },
     },
     data() {
         return {
@@ -47,9 +121,9 @@ export default {
             .get("https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas/" + this.programId + "?form=json")
             .then((response) => {
                 this.idData = response.data;
-                
+            
                 //Error Handling
-                // Tried beforeEnter, and beforeRouteEnter, but wont work with Axios. 
+                // Tried beforeEnter, and beforeRouteEnter, but it wont work with Axios. 
                 // It is needed to check the middleware if the ID is true, that the error is handled with a v-if inside the component.
                 if (response.data.responseCode === 404) {
                     //Should the route have changed to something that can not be found after finding a program, then we need to reset foundData.
