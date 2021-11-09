efb4ab212993a27b7784aa6cdb35f3330df27a26
Anders <1086088@ucn.dk>
2021-11-10 00:00:41 +0100

    Work on Wishlist page

--- src/views/ProgramsTemplate.vue
+++ src/views/ProgramsTemplate.vue
@@ -1,14 +1,18 @@
 <template>
   <main>
-    <section class="introToGenre">
+    <section v-if="checkGenreId == 0" class="introToGenre">
       <h1>You have choosen the genre {{ genreTitle }}!</h1>
+      {{checkGenreId}}
     </section>
-    <display-all :currentGenre="genreId"></display-all>
+    <display-all v-if="checkGenreId == 0" :currentGenre="genreId"></display-all>
+    <!-- Error handling -->
+    <error-get v-if="checkGenreId.status == true" :errorObject="checkGenreId.error"></error-get>
   </main>
 </template>
 
 <script>
 import Display from "../components/programs/ProgramsProgramDisplay.vue"
+import ErrorGet from "../components/global/ErrorGET.vue"
 export default {
   name: "ProgramsTemplate",
   props: {
@@ -18,12 +22,36 @@ export default {
     },
   },
   components: {
-      "display-all": Display
+      "display-all": Display,
+      "error-get": ErrorGet
+  },
+  data() {
+    return {
+      genres: ["action", "comedy", "thriller", "war", "romance", "drama", "crime", "documentary", "horror"],
+      error: false,
+    }
   },
   computed: {
     genreTitle: function () {
       return this.genreId.charAt(0).toUpperCase() + this.genreId.slice(1);
     },
+    checkGenreId: function () {
+      let check = this.genres.indexOf(this.genreId);
+      
+      if(check == -1) {
+        let obj = {
+          status: true,
+          error: {
+            title: "Homepage",
+            description: "This genre does not exist in our database.",
+            returnLink: "/"
+          }
+        };
+        return obj;
+      } else {
+        return check;
+      }
+    }
   },
   methods: {
    
--- src/views/UserWishlist.vue
+++ src/views/UserWishlist.vue
@@ -1,13 +1,57 @@
 <template>
   <main>
-    
+    <section class="intro">
+      <h1>This is your very own Wishlist!</h1>
+    </section>
+
+    <section>
+      <info-box v-for="wish in GETarray" :key="wish.id" :program="wish"></info-box>
+    </section>
   </main>
 </template>
 
 <script>
+import ProgramInfoBox from "../components/global/ProgramInfobox.vue"
+import axios from "axios"
 export default {
-    name: "UserWishlist",
-    
+  name: "UserWishlist",
+  components: {
+    "info-box": ProgramInfoBox,
+  },
+  data() {
+    return {
+      wishlist: [],
+      GETarray: [],
+    };
+  },
+  created() {
+    if (localStorage.getItem("wishlist")) {
+      // Check if the wishId is in the list and if the wishlist exists.
+      // First we are trying to parse the string from local storage to JSON, if not the localStorage wishlist should be removed.
+      try {
+        this.wishlist = JSON.parse(localStorage.getItem("wishlist"));
+      } catch (err) {
+        //Remove from localStorage and return false, because the wishlist existed but was either empty
+        localStorage.removeItem();
+      }
+    }
+
+    this.GETall();
+  },
+  methods: {
+    GETall() {
+      this.wishlist.forEach((wish) => {
+        axios
+        .get("https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas/" + wish +"?form=json&fields=id,title,plprogram$thumbnails")
+        .then((response) => {
+          this.GETarray.push(response.data);
+        })
+        .catch(function (error) {
+          console.log(error)
+        });
+      });
+    },
+  },
 };
 </script>
 
