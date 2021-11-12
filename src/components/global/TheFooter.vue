caacfe9a83d913e91b26820019cfdddd4b061491
ucn-aen <anderseriknissen@gmail.com>
2021-11-12 10:20:47 +0100

    Create TheFooter.vue

---
+++ src/components/global/TheFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+        <section>
+            <!-- Genre List -->
+        </section>
+
+        <section>
+            <!-- Navigation? -->
+        </section>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "TheFooter",
+    props: {
+        genreArray: {
+            type: Array,
+        }
+    },
+    data() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
